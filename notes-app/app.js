35a2b4ae39bed93f126bb14c7b7ecdd71c983130
vishkid84 <vishalittr@gmail.com>
2022-03-09 22:39:55 +0000

    installed npm validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,11 @@
+const validator = require('validator')
+
 const getNotes = require('./notes.js')
 
 const notes = getNotes()
 console.log(notes);
+
+console.log(validator.isURL('https://deckandweb.com'))
 
 // const add = require('./utils.js')
 // const sum = add(4, -2)
