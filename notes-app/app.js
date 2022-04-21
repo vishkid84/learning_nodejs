4240066f60e60b0d825d266f411312a7e6075ee6
vishkid84 <vishalittr@gmail.com>
2022-04-21 13:30:58 +0000

    getting input from user

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,30 @@
-const validator = require('validator')
-
+// const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const notes = getNotes()
-console.log(notes);
 
-console.log(validator.isURL('https://deckandweb.com'))
+console.log(process.argv)
+const command = process.argv[2] //argument vector, all the arguments in the node process
+
+if (command === 'add'){
+    console.log('Adding note')
+} else if(command === 'remove'){
+    console.log('Removing note')
+}
+
+
+
+
+// --------------------------------- 
+
+
+// const notes = getNotes()
+// console.log(notes);
+
+// console.log(chalk.bold.green.inverse('Success'))
+
+// console.log(validator.isURL('https://deckandweb.com'))
+// console.log(validator.isEmail('vishal@js.com'));
 
 // const add = require('./utils.js')
 // const sum = add(4, -2)
