97dad393f125b3006b9cd1ddda3a80cdfd496b43
vishkid84 <vishalittr@gmail.com>
2022-04-21 14:37:46 +0000

    installed yargs, argument parsing with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,22 +1,78 @@
-// const validator = require('validator')
+const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
-const command = process.argv[2] //argument vector, all the arguments in the node process
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
 
-if (command === 'add'){
-    console.log('Adding note')
-} else if(command === 'remove'){
-    console.log('Removing note')
-}
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List out a note',
+    handler: function(){
+        console.log('Listing out all note')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading the note')
+    }
+})
+
+yargs.parse()
+
+// console.log(yargs.argv)
+
+
+
 
 
 
 
 // --------------------------------- 
 
+// const command = process.argv[2] //argument vector, all the arguments in the node process
+// if (command === 'add'){
+//     console.log('Adding note')
+// } else if(command === 'remove'){
+//     console.log('Removing note')
+// }
 
 // const notes = getNotes()
 // console.log(notes);
