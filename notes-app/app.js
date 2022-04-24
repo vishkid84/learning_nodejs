5b6db0e8c7c8138917469414082950ed968c2c6f
vishkid84 <vishalittr@gmail.com>
2022-04-24 14:01:26 +0000

    storing data with json

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,7 @@
 const validator = require('validator')
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 // Customize yargs version
 yargs.version('1.1.0')
@@ -23,8 +23,7 @@ yargs.command({
         }
     },
     handler: function(argv){
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,9 +1,20 @@
-// const getNotes = function() {
-//     return 'Your notes ...'
-// }
+const fs = require ('fs')
 
 const getNotes = () =>{
     return 'Your notes ...'
 }
 
-module.exports = getNotes;
+const addNote = function(title, body){
+    const notes = loadNotes()
+}
+
+const loadNotes = () =>{
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJason = dataBuffer.toString()
+    return JSON.parse(dataJason)
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,   
+}
